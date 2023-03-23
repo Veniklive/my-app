0bd7fe3adf003b8067cb6e394f23dcf96d55d756
Veniklive <danilvivivi@gmail.com>
2023-03-23 10:35:32 +0200

    Pub

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import axios from 'axios';
+import { Button, Glyphicon } from 'react-bootstrap';
 
-export default App;
+export default class App extends Component {
+   constructor() {
+     super();
+     this.state = {
+       id: ''
+     }
+     this.handleClick = this.handleClick.bind(this);
+  }
+
+   handleClick() {
+     axios.get('https://api.github.com/users/maecapozzi')
+       .then(response =>this.setState({id:response.data.id}))
+   }
+
+  render() {
+    return (
+      <div className='button_container' onClick={this.handleClick}>
+        <button className='button'>
+          push me
+        </button>
+         <p className='id'>{this.state.id}</p>
+      </div>
+    )
+  }
+}
