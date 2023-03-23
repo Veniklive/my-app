a7d35e232c54fefd1486ee92a45ec6e4b8b49d75
Veniklive <danilvivivi@gmail.com>
2023-03-23 12:40:14 +0200

    IDZcommit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 
 import axios from 'axios';
 import { Button, Glyphicon } from 'react-bootstrap';
+import { getElementError } from '@testing-library/react';
 
 export default class App extends Component {
    constructor() {
@@ -11,6 +12,9 @@ export default class App extends Component {
        id: ''
      }
      this.handleClick = this.handleClick.bind(this);
+
+     this.handleClickAddress = this.handleClickAddress.bind(this);
+
   }
 
    handleClick() {
@@ -18,13 +22,24 @@ export default class App extends Component {
        .then(response =>this.setState({id:response.data.id}))
    }
 
+   handleClickAddress() {
+   window.location.href=document.getElementById("address").value;
+  }
+
+
   render() {
     return (
-      <div className='button_container' onClick={this.handleClick}>
-        <button className='button'>
+      <div className='button_container'>
+        <button className='button' onClick={this.handleClick}>
           push me
         </button>
          <p className='id'>{this.state.id}</p>
+
+        <button className='button' onClick={this.handleClickAddress}>
+          To Adress
+        </button>
+         <input id='address' defaultValue='https://' type="Text"></input>
+
       </div>
     )
   }
